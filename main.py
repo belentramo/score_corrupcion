3ee3f68fd8d8078221637f65d4a2853ce70d1618
Belentramo <belen.tramolao@usach.cl>
2023-11-17 13:24:12 -0300

    agrego actualizaciones

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ df_filtered = df.query('Region == @filtro_region and Organismo == @filtro_organi
 st.dataframe(df_filtered)
 
 # Crear un histograma con Plotly Express
-fig = px.histogram(df_filtered, x='score_pred', nbins=20, title='Histograma de score_pred')# Mostrar el histograma
+#fig = px.histogram(df_filtered, x='score_pred', nbins=20, title='Histograma de score_pred')# Mostrar el histograma
 
+fig = px.histogram(df_filtered, x='score_pred', nbins=20, title='Histograma de score_pred')
+fig.update_layout(
+    title={
+        'text': "Histograma de score_pred",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    xaxis_title="Puntaje Score",
+    yaxis_title="Frecuencia",
+    plot_bgcolor='rgba(0,0,0,0)',
+    bargap=0.2,
+)
+fig.update_traces(marker_color='darkblue')
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
 st.plotly_chart(fig)
